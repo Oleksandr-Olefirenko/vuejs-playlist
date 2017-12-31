e2b672b69070bf44eeb71876407ca6ede52f158b
Aleks Saint <aleks.saint@gmail.com>
2017-12-31 19:03:43 +0200

    keep-alive for dynamic forms

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,31 @@
 <template>
   <div>
-      <app-header :title="title"></app-header>
-      <app-ninjas :ninjas="ninjas"></app-ninjas>
-      <app-footer :title="title"></app-footer>
-      <app-helper>
-        <div slot="form-head">
-          <h3>Title of the form</h3>
-          <p>info on the form</p>
-        </div>
-        <div slot="form-fields">
-          <input type="text" placeholder="name" required>
-          <input type="password" placeholder="password" required>
-        </div>
-        <div slot="form-controls">
-          <button @click="handleSubmit">Submit</button>
-        </div>
-      </app-helper>
+    <keep-alive>
+      <component :is="component"></component>
+    </keep-alive>
+    <button @click="component='form-one'">Show form one</button>
+    <button @click="component='form-two'">Show form two</button>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import Ninjas from './components/Ninjas.vue'
-//import formHelper from './components/formHelper.vue'
+
+import formOne from './components/formOne.vue'
+import formTwo from './components/formTwo.vue'
+import formHelper from './components/formHelper.vue'
 
 export default {
   name: 'app',
+  components: {
+    'form-one': formOne,
+    'form-two': formTwo
+  },
   data () {
     return {
-      ninjas: [
-              {name: 'Ryu', speciality: 'Vue Components', show: false},
-              {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-              {name: 'Hitoshi', speciality: 'Click Events', show: false},
-              {name: 'Tango', speciality: 'Conditionals', show: false},
-              {name: 'Kami', speciality: 'Webpack', show: false},
-              {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-          ],
-      title: 'Vue Wizzards'
-    }
-  },
-  components:{
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas,
-    'app-helper': formHelper,
-  },
-  methods: {
-    handleSubmit(){
-      alert('submit shot')
-    }
+      component: 'form-one'
     }
   }
+}
 </script>
 
 <style scoped>
--- src/components/formHelper.vue
+++ src/components/formHelper.vue
@@ -1,34 +1,39 @@
 <template>
-  <div>
-    <h1>Please fill in the form:</h1>
-    <form>
-      <div id="form-header">
-        <slot name='form-head'></slot>
-      </div>
-      <div id="form-fields">
-        <slot name='form-fields'></slot>
-      </div>
-      <div id="form-controls">
-        <slot name='form-controls'></slot>
-      </div>
-      <div id="useful-links">
-        <ul>
-          <li><a href="#">Link 1</a></li>
-          <li><a href="#">Link 2</a></li>
-          <li><a href="#">Link 3</a></li>
-        </ul>
-      </div>
-    </form>
-  </div>
+    <div>
+    <h1>Please fill out our form...</h1>
+        <form>
+            <div id="form-header">
+                <slot name="form-header"></slot>
+            </div>
+            <div id="form-fields">
+                <slot name="form-fields"></slot>
+            </div>
+            <div id="form-controls">
+                <slot name="form-controls"></slot>
+            </div>
+            <div id="useful-links">
+                <ul>
+                    <li><a href="#">link 1</a></li>
+                    <li><a href="#">link 2</a></li>
+                    <li><a href="#">link 3</a></li>
+                    <li><a href="#">link 4</a></li>
+                </ul>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
-import {bus} from '../main'
+// Imports
 export default {
-  data () {
-    return {
+    components: {
+    },
+    data () {
+        return {
+        }
+    },
+    methods: {
     }
-  },
 }
 </script>
 
