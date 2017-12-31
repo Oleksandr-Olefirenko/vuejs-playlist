ba221287513185b2d79aa78ad3fa84c6c51b0373
Aleks Saint <aleks.saint@gmail.com>
2017-12-31 19:07:05 +0200

    Starting blog app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div>
-    <keep-alive>
-      <component :is="component"></component>
-    </keep-alive>
-    <button @click="component='form-one'">Show form one</button>
-    <button @click="component='form-two'">Show form two</button>
-  </div>
+
 </template>
 
 <script>
 
-import formOne from './components/formOne.vue'
-import formTwo from './components/formTwo.vue'
-import formHelper from './components/formHelper.vue'
 
 export default {
   name: 'app',
-  components: {
-    'form-one': formOne,
-    'form-two': formTwo
-  },
+
   data () {
     return {
-      component: 'form-one'
+
     }
   }
 }
