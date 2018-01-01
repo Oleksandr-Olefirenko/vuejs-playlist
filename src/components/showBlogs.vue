b49b146f9002755679a71c6185f376faa2c229d3
Aleks Saint <aleks.saint@gmail.com>
2018-01-02 00:58:15 +0200

    Added computed filter (amazing performance)

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,7 +1,8 @@
 <template>
   <div v-theme:column="'narrow'" id="show-blogs">
     <h1>All blog Articles</h1>
-    <div v-for="blog in blogs" class="single-blog">
+    <input type="text" v-model="search" placeholder="search blogs"/>
+    <div v-for="blog in filteredBlogs" class="single-blog">
       <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
       <article>
         {{blog.body | snippet }}
@@ -15,7 +16,15 @@ export default {
 
   data () {
     return {
-      blogs: []
+      blogs: [],
+      search: ''
+    }
+  },
+  computed: {
+    filteredBlogs: function(){
+      return this.blogs.filter((blog)=>{
+        return blog.title.match(this.search)
+      })
     }
   },
   created(){
