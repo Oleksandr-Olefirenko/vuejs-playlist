ab2780f1a4793777ccc50f39436ad372c7d47af3
Aleks Saint <aleks.saint@gmail.com>
2018-01-02 00:39:42 +0200

    Added custom filters

--- src/main.js
+++ src/main.js
@@ -27,6 +27,14 @@ Vue.directive('theme', {
   }
 })
 
+//custom filters
+Vue.filter('to-uppercase', function(value) {
+  return value.toUpperCase()
+})
+Vue.filter('snippet', function(value) {
+  return value.slice(0, 100) +'...'
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
