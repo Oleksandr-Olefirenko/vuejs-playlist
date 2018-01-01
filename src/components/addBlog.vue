ba4c926a2ae833bdd6f5ec37ca4390b5ef964976
Aleks Saint <aleks.saint@gmail.com>
2018-01-01 22:54:47 +0200

    added all elements, going to requests!

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -16,6 +16,10 @@
         <label>Cheese</label>
         <input type="checkbox" value="cheese" v-model="blog.categories" />
       </div>
+      <label>Author:</label>
+      <select v-model="blog.author">
+        <option v-for="author in authors">{{author}}</option>
+      </select>
     </form>
     <div id="preview">
       <h3>Blog Preview</h3>
@@ -26,6 +30,7 @@
       <ul>
         <li v-for="cat in blog.categories">{{cat}}</li>
       </ul>
+      <p>Author: {{blog.author}}</p>
     </div>
   </div>
 </template>
@@ -40,7 +45,9 @@ export default {
         title: '',
         content: '',
         categories: [],
-      }
+        author: '',
+      },
+      authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicatior']
     }
   }
 }
